9bb5ec62b4a705a77a0d5d372bddcd42eed7a95c
hieunm292 <nmhieu2902@gmail.com>
2021-07-23 14:02:41 +0700

    Init Project

---
+++ app/jobs/application_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ApplicationJob < ActiveJob::Base
+  # Automatically retry jobs that encountered a deadlock
+  # retry_on ActiveRecord::Deadlocked
+
+  # discard_on ActiveJob::DeserializationError
+end
