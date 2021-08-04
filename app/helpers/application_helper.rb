6f176207d3378de67d445d8ca249c0b5df9def03
hieunm292 <nmhieu2902@gmail.com>
2021-08-05 00:47:26 +0700

    static-pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title page_title
+    base_title = t ".title_global"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
