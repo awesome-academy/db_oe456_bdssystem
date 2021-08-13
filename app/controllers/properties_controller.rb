d32ea78385048b404dacb1f9b4b1b98bc251b29d
hieunm292 <nmhieu2902@gmail.com>
2021-08-13 18:07:45 +0700

    Upload_Image_crud_Property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,63 @@
 class PropertiesController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create,:edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @properties = Property.newest.page(params[:page]).per(Settings.account.per_page)
+    @properties = Property.newest
+                          .page(params[:page])
+                          .per(Settings.account.per_page)
+  end
+
+  def show; end
+
+  def new
+    @property = Property.new
+  end
+
+  def edit; end
+
+  def create
+    @property = current_account.properties.build(property_params)
+    @property.photo.attach(params[:property][:photo])
+    if @property.save
+      flash[:success] = t ".property_create_success"
+      redirect_to @property
+    else
+      flash[:danger] = t ".create_property_failed"
+      render :new
+    end
+  end
+
+  def update
+    if @property.update(property_params)
+      flash[:success] = t ".property_update_success"
+      redirect_to properties_url
+    else
+      flash[:danger] = t ".update_property_failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @property.destroy
+      flash[:success] = t ".property_destroy_success"
+    else
+      flash[:danger] = t ".property_destroy_fail"
+    end
+    redirect_to properties_url
+  end
+
+  private
+  def property_params
+    params.require(:property).permit(:name, :address, :price, :rooms,
+                                     :bathrooms, :photo)
+  end
+
+  def correct_user
+    @property = current_account.properties.find_by id: params[:id]
+    return if @property
+
+    flash[:warning] = t ".invalid_account"
+    redirect_to request.referer || root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_070049) do
+ActiveRecord::Schema.define(version: 2021_08_13_075828) do
 
   create_table "accounts", charset: "utf8mb3", force: :cascade do |t|
     t.string "last_name"
@@ -21,6 +21,34 @@ ActiveRecord::Schema.define(version: 2021_08_11_070049) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.index ["email"], name: "index_accounts_on_email", unique: true
+  end
+
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "properties", charset: "utf8mb3", force: :cascade do |t|
@@ -36,5 +64,7 @@ ActiveRecord::Schema.define(version: 2021_08_11_070049) do
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "properties", "accounts"
 end
